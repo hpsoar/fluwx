c4753df7e3d5f8c32e46ea6767ae5345ee02a656
Huang Peng <huangpeng@chunyu.me>
2020-03-29 11:06:32 +0800

    MOD: ..

--- android/src/main/kotlin/com/jarvan/fluwx/FluwxPlugin.kt
+++ android/src/main/kotlin/com/jarvan/fluwx/FluwxPlugin.kt
@@ -22,6 +22,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class FluwxPlugin(private val registrar: Registrar, channel: MethodChannel) : MethodCallHandler {
